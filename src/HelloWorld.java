79923abcb8d17aed51a4b752992c0aff054c3241
Kasia <kasia@wiseman.pl>
2016-12-13 19:36:25 +0100

    Utworzenie projektu

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Kasia on 13.12.2016.
+ */
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hej ho");
+        System.out.println(255);
+    }
+}
