f44efeb001a63b60d7bc03c0c591d83c6cf635e1
Kasia <kasia@wiseman.pl>
2016-12-15 10:31:43 +0100

    zadanie domowe

--- src/Zadanie1.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Kasia on 14.12.2016.
- */
-public class Zadanie1 {
-    public static void main(String[] args) {
-        System.out.println("Uczę\nsię\nprogramować");
-    }
-
-}
